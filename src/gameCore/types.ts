3ba7241f0b533552d492dad7a589134bd9a699e7
nbogie <neillbogie@googlemail.com>
2022-07-20 23:23:57 +0100

    export some needed types

--- src/gameCore/types.ts
+++ src/gameCore/types.ts
@@ -1,5 +1,5 @@
-type MovementAmount = 1 | 2 | 3 | 4;
-type Direction = "left" | "right";
+export type MovementAmount = 1 | 2 | 3 | 4;
+export type Direction = "left" | "right";
 
 export interface Movement {
   direction: Direction;
